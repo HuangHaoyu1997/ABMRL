3384016aa1b3e6b93b16a2232eef6c48f5e4f71d
Haoyu Huang <446700679@qq.com>
2021-11-09 15:10:03 +0800

    33

---
+++ neighbor.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def neighbor(xy,map_size,val_map,use_map,agent_pool,offset=5):
+    '''
+    计算邻里平均经济状况
+    也就是xy周围一定范围内的土地/智能体的价值之平均
+    若地块空闲，则计算地价，若地块有人居住，则计算智能体收入
+    
+    '''
+
+    def is_agent(xy,use_map):
+        '''
+        判断当前地块是空地还是被智能体占据
+        ID > 999是ID号
+        ID = 0是空地
+        '''
+        x,y = xy
+        ID = use_map[x,y]
+        if ID > 999:   return ID
+        elif ID < 999: return 0
+
+    x,y = xy
+
+    x_offset, y_offset = offset, offset
+    dir = []
+    for x in range(-x_offset,x_offset):
+        for y in range(-y_offset,y_offset):
+            dir.append([x,y])
+    dir.pop(int(0.5*2*x_offset*2*y_offset+y_offset)) # 刨除(0,0)点
+    
+    # dir = meshgrid(offset=[offset,offset])
+    sum = []
+    for off_x, off_y in dir:
+        if ((x+off_x) >= 0) and ((x+off_x)<map_size[0]) and ((y+off_y) >= 0) and ((y+off_y)<map_size[1]): # 不越界
+            if use_map[x+off_x,y+off_y] >= 0: # 能访问
+                id = is_agent([x+off_x,y+off_y],use_map) # 查找该地块上有没有人居住
+                if id >= 1000: # 1000以上的id代表agent
+                    sum.append(agent_pool[id].income) # 收入
+                elif id < 1000: # 1000以下的id代表空地
+                    sum.append(val_map[x+off_x,y+off_y]) # 地价
+    return np.mean(sum)
--- simulator.py
+++ simulator.py
@@ -9,6 +9,146 @@ from numpy.matrixlib import defmatrix
 from agent import *
 from grid import *
 import time
+from neighbor import neighbor
+
+def is_agent(xy,use_map):
+    '''
+    判断当前地块是空地还是被智能体占据
+    ID > 999是ID号
+    ID = 0是空地
+    '''
+    x,y = xy
+    ID = use_map[x,y]
+    if ID > 999:   return ID
+    elif ID < 999: return 0
+
+def meshgrid(offset=[10,10],pop=True):
+    '''
+    生成类似于如下的矩阵
+    ((-1,-1),(-1,0),(-1,1),
+    (0,-1),         (0,1),
+    (1,-1),(1,0),(1,1))
+    '''
+    x_offset, y_offset = offset
+    dir = []
+    for x in range(-x_offset,x_offset):
+        for y in range(-y_offset,y_offset):
+            dir.append([x,y])
+    if pop:
+        dir.pop(int(0.5*2*x_offset*2*y_offset+y_offset)) # 刨除(0,0)点
+
+    return dir
+
+def location_effect(ID,xy,agent_pool,work_xy,tra_xy,val_map,
+                        use_map,map_size,wg,ws,c1,c2):
+    '''
+    计算区位效应
+    U = wg * G + ws * (1 - S) + e
+    G是外部吸引力
+    S是内部压力
+    e是随机变量
+    xy可以是ID的坐标，也可以不是
+    因此可以计算给定位置的Agent与另一块土地的假设区位效应
+    
+    '''
+    
+    weight = agent_pool[ID].weight # 智能体的权重
+    work_id = agent_pool[ID].work_id
+    work_xy = work_xy[work_id]
+    G = cal_out_pressure(xy, 
+                        work_xy, 
+                        tra_xy, 
+                        val_map, 
+                        weight).sum() # 外部居住环境吸引力
+    S = cal_in_pressure(ID,
+                        xy,
+                        agent_pool,
+                        val_map,
+                        use_map,
+                        map_size,
+                        c1,
+                        c2
+                        ) # ID智能体在xy位置的内部压力
+    LocationEffect = wg*G + ws*(1-S) + 0.1*np.random.rand()
+    # print("内部压力:",S)
+    return LocationEffect 
+
+def cal_out_pressure(xy, work_xy, tra_xy, val_map, weight):
+    '''
+    计算给定坐标的外部居住环境吸引力
+    G_h^t = w_env*E_env + w_edu*E_edu + w_tra*E_tra + w_pri*E_pri + w_con*E_con
+    Agent权重的排序: 交通、通勤、地价
+    '''
+    
+    '''环境、教育、基础设施
+    E_env = np.min(np.sqrt(np.sum((self.grid.env_xy-xy)**2,1)))
+    E_env = np.exp(1-0.001*E_env) # 指数距离衰减函数
+
+    E_edu = np.min(np.sqrt(np.sum((self.grid.edu_xy-xy)**2,1)))
+    E_edu = np.exp(1-0.001*E_edu) # 指数距离衰减函数
+    
+    E_inf = np.min(np.sqrt(np.sum((self.grid.inf_xy-xy)**2,1)))
+    E_inf = np.exp(1-0.001*E_inf) # 指数距离衰减函数
+    '''
+    x,y = xy
+    # 交通        
+    E_tra = np.min(np.sqrt(np.sum((tra_xy-xy)**2,1))) # 与最近地铁站点的距离
+    E_tra = np.exp(1-0.001*E_tra) # 指数距离衰减函数
+    
+    # 通勤
+    E_work = np.sqrt(np.sum((work_xy-xy)**2)) # 和指定的企业计算通勤距离
+    # E_work = np.sum(np.abs(work_xy-xy)) # 曼哈顿距离
+
+    # 房价
+    # 实际上外部吸引力与房价并非正比关系，而是对不同人群有不同影响，人们可能更偏好比当前收入稍好一点的房屋，但不喜欢房价高出很多的房屋
+    E_price = 0.001 * val_map[x,y] 
+    return weight * np.array([E_tra,E_work,E_price])
+
+def cal_in_pressure(ID, xy, agent_pool, val_map, use_map, map_size, c1, c2):
+    '''
+    计算内部社会经济压力
+    S_h^t = c1*|I_h^t - V_h^t| + c2*|I_h^t - P_h^t|
+    S是社会经济压力
+    I是个人收入
+    V是所占据土地的价值
+    P是邻居平均经济状况
+    c1、c2是系数
+    '''
+    x,y = xy
+    income = agent_pool[ID].income
+    price = val_map[x,y] # 所占土地地价
+    P = neighbor(xy,
+                map_size,
+                val_map,
+                use_map,
+                agent_pool) # 邻里平均经济状况
+    S = c1 * np.abs(income-price) + c2 * np.abs(income-P)
+    # print('income,',income,price,P,S)
+    return S
+
+def neighbor_value(xy,use_map,val_map,map_size,offset=10):
+    '''
+    计算周围土地的价值
+    input：xy坐标
+    output：地价list和均值
+    '''
+    x,y = xy
+    dir = meshgrid(offset=[offset,offset])
+    value,count = 0,0
+    # n_value = []
+    for off_x,off_y in dir:
+        if  ((x+off_x) >= 0)        and \
+            ((x+off_x)<map_size[0]) and \
+            ((y+off_y) >= 0)        and \
+            ((y+off_y)<map_size[1]) and \
+            (use_map[x+off_x,y+off_y] >= 0): # 不越界,能访问
+            
+            value += val_map[x+off_x,y+off_y]
+            count += 1
+            # n_value.append(self.grid.val_map[x+off_x,y+off_y])
+
+    return value/count # n_value, np.mean(n_value)
+
 class env:
     def __init__(self) -> None:
         self.map_size = [100,100]
@@ -110,28 +250,7 @@ class env:
                                 [0.5,0.75],
                                 [0.75,1.0]]) * max_income
 
-    def cal_in_pressure(self, ID, xy):
-        '''
-        计算内部社会经济压力
-        S_h^t = c1*|I_h^t - V_h^t| + c2*|I_h^t - P_h^t|
-        S是社会经济压力
-        I是个人收入
-        V是所占据土地的价值
-        P是邻居平均经济状况
-        c1、c2是系数
-        '''
-        x,y = xy
-        income = self.agent_pool[ID].income
-        price = self.grid.val_map[x,y] # 所占土地地价
-        P = self.neighbor(xy) # 邻里平均经济状况
-        S = self.c1 * np.abs(income-price) + self.c2 * np.abs(income-P)
-        # print('income,',income,price,P,S)
-        return S
-
-    
-
-
-    @nb.jit()
+    # @nb.jit()
     def occupation(self,xy,offset=5):
         '''
         计算邻域内被占据的格点数（已出租房屋）占比
@@ -140,7 +259,7 @@ class env:
         output：已占据格点数/总格点数
         '''
         x,y = xy
-        dir = self.meshgrid(offset=[offset,offset])
+        dir = meshgrid(offset=[offset,offset])
         n_occupied, n_total = 0, 0
         for off_x,off_y in dir:
             if ((x+off_x) >= 0) and ((x+off_x)<self.map_size[0]) and ((y+off_y) >= 0) and ((y+off_y)<self.map_size[1]): # 不越界
@@ -149,85 +268,6 @@ class env:
                     if self.grid.use_map[x+off_x,y+off_y] >= 1000: # 1000以上的id代表agent
                         n_occupied += 1
         return n_occupied / n_total
-
-        
-    
-
-    @nb.jit()
-    def neighbor_value(self,xy,offset=10):
-        '''
-        计算周围土地的价值
-        input：xy坐标
-        output：地价list和均值
-        '''
-        x,y = xy
-        dir = self.meshgrid(offset=[offset,offset])
-        value,count = 0,0
-        # n_value = []
-        for off_x,off_y in dir:
-            if ((x+off_x) >= 0)              and \
-                ((x+off_x)<self.map_size[0]) and \
-                ((y+off_y) >= 0)             and \
-                ((y+off_y)<self.map_size[1]) and \
-                (self.grid.use_map[x+off_x,y+off_y] >= 0): # 不越界,能访问
-                
-                value += self.grid.val_map[x+off_x,y+off_y]
-                count += 1
-                # n_value.append(self.grid.val_map[x+off_x,y+off_y])
-
-        return value/count # n_value, np.mean(n_value)
-
-    def cal_out_pressure(self, xy, work_xy, weight):
-        '''
-        计算给定坐标的外部居住环境吸引力
-        G_h^t = w_env*E_env + w_edu*E_edu + w_tra*E_tra + w_pri*E_pri + w_con*E_con
-        Agent权重的排序: 交通、通勤、地价
-        '''
-        
-        '''环境、教育、基础设施
-        E_env = np.min(np.sqrt(np.sum((self.grid.env_xy-xy)**2,1)))
-        E_env = np.exp(1-0.001*E_env) # 指数距离衰减函数
-
-        E_edu = np.min(np.sqrt(np.sum((self.grid.edu_xy-xy)**2,1)))
-        E_edu = np.exp(1-0.001*E_edu) # 指数距离衰减函数
-        
-        E_inf = np.min(np.sqrt(np.sum((self.grid.inf_xy-xy)**2,1)))
-        E_inf = np.exp(1-0.001*E_inf) # 指数距离衰减函数
-        '''
-        x,y = xy
-        # 交通        
-        E_tra = np.min(np.sqrt(np.sum((self.grid.tra_xy-xy)**2,1))) # 与最近地铁站点的距离
-        E_tra = np.exp(1-0.001*E_tra) # 指数距离衰减函数
-        
-        # 通勤
-        E_work = np.sqrt(np.sum((work_xy-xy)**2)) # 和指定的企业计算通勤距离
-        # E_work = np.sum(np.abs(work_xy-xy)) # 曼哈顿距离
-
-        # 房价
-        # 实际上外部吸引力与房价并非正比关系，而是对不同人群有不同影响，人们可能更偏好比当前收入稍好一点的房屋，但不喜欢房价高出很多的房屋
-        E_price = 0.001 * self.grid.val_map[x,y] 
-        return weight * np.array([E_tra,E_work,E_price])
-
-    def location_effect(self,ID,xy):
-        '''
-        计算区位效应
-        U = wg * G + ws * (1 - S) + e
-        G是外部吸引力
-        S是内部压力
-        e是随机变量
-        xy可以是ID的坐标，也可以不是
-        因此可以计算给定位置的Agent与另一块土地的假设区位效应
-        
-        '''
-        
-        weight = self.agent_pool[ID].weight # 智能体的权重
-        work_id = self.agent_pool[ID].work_id
-        work_xy = self.grid.work_xy[work_id]
-        G = self.cal_out_pressure(xy, work_xy, weight).sum() # 外部居住环境吸引力
-        S = self.cal_in_pressure(ID, xy) # ID智能体在xy位置的内部压力
-        LocationEffect = self.wg*G + self.ws*(1-S) + 0.1*np.random.rand()
-        # print("内部压力:",S)
-        return LocationEffect 
 
     def move(self,ID):
         '''
@@ -246,7 +286,7 @@ class env:
         '''
         xy = self.agent_pool[ID].coord
         x,y = xy
-        dir = self.meshgrid(offset=[self.move_step, self.move_step])
+        dir = meshgrid(offset=[self.move_step, self.move_step])
         is_occupied = []
         le = []
         for off_x,off_y in dir:
@@ -256,14 +296,36 @@ class env:
                 ((y+off_y)<self.map_size[1]): # 不越界
 
                 if self.grid.use_map[x+off_x,y+off_y] >= 0: # 是可用地块
-                    id = self.is_agent([x+off_x,y+off_y])
+                    id = is_agent([x+off_x,y+off_y],self.grid.use_map)
                     if id <= 999: # 空地
-                        L_E = self.location_effect(ID,[x+off_x,y+off_y]) # ID智能体在[x+off_x,y+off_y]位置的区位效应
+                        L_E = location_effect(ID,
+                                            [x+off_x,y+off_y],
+                                            self.agent_pool,
+                                            self.grid.work_xy,
+                                            self.grid.tra_xy,
+                                            self.grid.val_map,
+                                            self.grid.use_map,
+                                            self.map_size,
+                                            self.wg,
+                                            self.ws,
+                                            self.c1,
+                                            self.c2) # ID智能体在[x+off_x,y+off_y]位置的区位效应
                         # print('Location Effect:',L_E)
                         le.append(L_E)
                         is_occupied.append([x+off_x,y+off_y])
         max_le = np.max(le)
-        AW = max_le - self.location_effect(ID,xy)
+        AW = max_le - location_effect(ID,
+                                    xy,
+                                    self.agent_pool,
+                                    self.grid.work_xy,
+                                    self.grid.tra_xy,
+                                    self.grid.val_map,
+                                    self.grid.use_map,
+                                    self.map_size,
+                                    self.wg,
+                                    self.ws,
+                                    self.c1,
+                                    self.c2)
         if AW >= self.WT: # 超过迁居阈值
             # print(le)
             prob = self.softmax(le)
@@ -285,7 +347,7 @@ class env:
         xx = np.exp(x)/(np.exp(x).sum())
         return xx
     
-    @nb.jit()
+    # @nb.jit()
     def update_value(self):
         '''
         改变土地价值
@@ -339,7 +401,11 @@ class env:
                 if self.grid.use_map[x,y] >= 0: # 可访问地块
                     t1 = time.time()
                     history_value = self.grid.val_map[x,y]
-                    neighbor_value = self.neighbor_value([x,y],offset=self.move_step)
+                    n_value = neighbor_value([x,y],
+                                            self.grid.use_map,
+                                            self.grid.val_map,
+                                            self.map_size,
+                                            offset=self.move_step)
                     t2 = time.time()
                     neighbor_occupy = self.occupation([x,y],offset=self.move_step) # 计算邻域已被占据的格点数，offset=10
                     t3 = time.time()
@@ -349,8 +415,8 @@ class env:
                     elif neighbor_occupy >= 0.2 and neighbor_occupy < 0.4:  factor = 0.95
                     elif neighbor_occupy >= 0.0 and neighbor_occupy < 0.2:  factor = 0.90
                     t4 = time.time()
-                    print('%.7f,%.7f,%.7f'%((t2-t1)*1000000.,(t3-t2)*1000000.,(t4-t3)*1000000.))
-                    new_value = factor * (0.5*history_value + 0.5*neighbor_value)
+                    # print('%.7f,%.7f,%.7f'%((t2-t1)*1000000.,(t3-t2)*1000000.,(t4-t3)*1000000.))
+                    new_value = factor * (0.5*history_value + 0.5*n_value)
                     self.grid.val_map[x,y] = new_value
 
     def gen_agent(self, N):
@@ -409,54 +475,6 @@ class env:
             new_figure[x,y,:] = [50,170,220] # 标注地铁站
         
         return new_figure
-
-def is_agent(xy,use_map):
-    '''
-    判断当前地块是空地还是被智能体占据
-    ID > 999是ID号
-    ID = 0是空地
-    '''
-    x,y = xy
-    ID = use_map[x,y]
-    if ID > 999:   return ID
-    elif ID < 999: return 0
-
-def meshgrid(offset=[10,10],pop=True):
-    '''
-    生成类似于如下的矩阵
-    ((-1,-1),(-1,0),(-1,1),
-    (0,-1),         (0,1),
-    (1,-1),(1,0),(1,1))
-    '''
-    x_offset, y_offset = offset
-    dir = []
-    for x in range(-x_offset,x_offset):
-        for y in range(-y_offset,y_offset):
-            dir.append([x,y])
-    if pop:
-        dir.pop(int(0.5*2*x_offset*2*y_offset+y_offset)) # 刨除(0,0)点
-
-    return dir
-
-def neighbor(xy,map_size,val_map,use_map,agent_pool,offset=5):
-    '''
-    计算邻里平均经济状况
-    也就是xy周围一定范围内的土地/智能体的价值之平均
-    若地块空闲，则计算地价，若地块有人居住，则计算智能体收入
-    
-    '''
-    x,y = xy
-    dir = meshgrid(offset=[offset,offset])
-    sum = []
-    for off_x, off_y in dir:
-        if ((x+off_x) >= 0) and ((x+off_x)<map_size[0]) and ((y+off_y) >= 0) and ((y+off_y)<map_size[1]): # 不越界
-            if use_map[x+off_x,y+off_y] >= 0: # 能访问
-                id = is_agent([x+off_x,y+off_y]) # 查找该地块上有没有人居住
-                if id >= 1000: # 1000以上的id代表agent
-                    sum.append(agent_pool[id].income) # 收入
-                elif id < 1000: # 1000以下的id代表空地
-                    sum.append(val_map[x+off_x,y+off_y]) # 地价
-    return np.mean(sum)
 
 
 if __name__ == '__main__':
